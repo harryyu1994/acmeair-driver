67121a1e462fca76dcc8a30a705e64e224eb3258
Harry Yu <harryyu1994@gmail.com>
2019-04-24 15:31:00 -0700

    Fix apply load bug

--- run_jmeter.sh
+++ run_jmeter.sh
@@ -1,1 +1,5 @@
+curl "http://localhost:80/booking/loader/load"
+curl "http://localhost:80/flight/loader/load"
+curl "http://localhost:80/customer/loader/load?numCustomers=10000"
+
 docker run -it --rm -v $PWD/jmeter_output:/output -e JHOST=localhost -e JPORT=80 -e JTHREAD=10 -e JDURATION=600 -e JUSER=199 --name jmeter_acmeair jmeter_acmeair
